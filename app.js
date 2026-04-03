1d85696a8b0aeeb83af4298633fcb1eead72089e
Daniel Botnarenco <danielbotnarenco@Daniels-MacBook-Pro.local>
2026-04-03 13:27:39 +0300

    initial map viewer

---
+++ app.js
@@ -0,0 +1,5 @@
+const map = L.map('map').setView([54, 15], 4);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
